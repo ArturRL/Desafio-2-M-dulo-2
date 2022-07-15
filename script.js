0198e112fa02c167eff0230f0924773dec921859
ArturRL <artur.rod.lopes@gmail.com>
2022-07-15 19:49:42 -0300

    Desafio entregue

---
+++ script.js
@@ -0,0 +1,54 @@
+//if(){
+
+//}
+//else{}
+
+let number= 18
+if(number>=18){
+    console.log("Você é maior de idade!")
+}
+else{
+    console.log("Você é menor de idade")
+}
+
+//
+
+
+let number2=40
+let MIB= true
+if((number2=40)&&(MIB=true)){
+    console.log("Você é humana!")
+}
+else{
+    console.log("Você não é humana!")
+}
+
+//=(receber), ==(igual), ===(igualzaço), &&("e"), ||("ou")
+
+let month="february"
+if((month==="february")||(month==="january")){
+    console.log("Parabennsss mané!")
+}
+else{
+    console.log("Calma aí pô!")
+}
+
+//
+
+let nome="Danda"
+if (nome==="Danda"){
+    console.log("Seu nome começa com a letra D")
+}
+else{
+    console.log("Tua mãe errou teu nome")
+}
+
+//
+
+let sobrenome="Pereira"
+if (sobrenome.length>=6 || sobrenome.substring(0,1)=="G"){
+    console.log("Seu sobrenome tem 6 letras e começa com a letra G")   
+}
+else{
+    console.log("Seu sobrenome não tem 6 letras e não começa com a letra G")   
+}
